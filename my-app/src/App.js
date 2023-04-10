7b6d5050d4c67b2fcb81e633cf6be43ab75043b6
Greg Stickle <gregorystickle@gmail.com>
2023-04-09 22:10:48 -0400

    Removed bloat code

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
 
 function App() {
+  let password = ""
+  
+  const handleClick = () => {
+    password = ""
+    let passwordChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcedfghijklmnopqrstuvwxy1234567890!?@#$%&"
+    for (let index = 0; index < 9; index++) {
+      password += passwordChar.charAt(Math.floor(Math.random() * passwordChar.length));
+    }
+    alert(`Password: ${password}`)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="content">
+        <h1>Password Generator</h1>
+        <button onClick={handleClick}>Generate Password</button>
+        <p>{password}</p>
+      </div>
     </div>
   );
 }
